e887d4e5b61ad3f504cb5c25e11f595ee6db76aa
b08902043 <56215453+b08902043@users.noreply.github.com>
2023-04-15 22:35:44 +0800

    login.php

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+include("config.php");
+session_start();
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+$myusername = mysqli_real_escape_string($db, $_POST['username']);
+$mypassword = mysqli_real_escape_string($db, $_POST['password']);
+$sql = "SELECT * FROM radcheck WHERE username='$myusername' and value='$mypass>
+$result = mysqli_query($db, $sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count = mysqli_num_rows($result);
+if($count >=  1)
+{
+$_SESSION['login_user'] = $myusername;
+echo "login sucess!";
+}
+else
+{
+$error = "Your Login Name or Password is invalid";
+echo $error;
+}
+}
+?>
+<h1>Login Here</h1>
+<form action="" method="POST">
+<label>Account:</label>
+<input type="text" name="username">
+<br>
+<label>Password:</label>
+<input type="password" name="password">
+<br>
+<input type="submit" value="Submit">
+<br>
+<br>
+<font size = "2">new user?</font>
+<br>
+<input type ="button" onclick="javascript:location.href='register.php'" value=>
+</form>
